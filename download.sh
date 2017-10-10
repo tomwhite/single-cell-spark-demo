8e197ebe30444851146828660c59fdfe7842e214
Tom White <tom@cloudera.com>
2017-10-10 12:43:31 +0100

    Add script to download data from https://support.10xgenomics.com/single-cell-gene-expression/datasets/1.3.0/1M_neurons

---
+++ download.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+curl -O http://cf.10xgenomics.com/samples/cell-exp/1.3.0/1M_neurons/1M_neurons_filtered_gene_bc_matrices_h5.h5
+curl -O http://cf.10xgenomics.com/samples/cell-exp/1.3.0/1M_neurons/1M_neurons_aggr_web_summary.html
+curl -O http://cf.10xgenomics.com/samples/cell-exp/1.3.0/1M_neurons/1M_neurons_reanalyze.csv
+curl -O http://cf.10xgenomics.com/samples/cell-exp/1.3.0/1M_neurons/1M_neurons_analysis.tar.gz
+curl -O http://cf.10xgenomics.com/samples/cell-exp/1.3.0/1M_neurons/1M_neurons_web_summary.html
+curl -O http://cf.10xgenomics.com/samples/cell-exp/1.3.0/1M_neurons/1M_neurons_cloupe.cloupe
+curl -O http://cf.10xgenomics.com/samples/cell-exp/1.3.0/1M_neurons/1M_neurons_neuron20k.h5
